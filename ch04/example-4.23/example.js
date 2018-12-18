0fb2fa2982428dff4912462a5f12308d88312b98
jiangyanxin <jiangyanxin1989@gmail.com>
2018-12-18 17:10:12 +0800

    update

--- ch04/example-4.23/example.js
+++ ch04/example-4.23/example.js
@@ -29,20 +29,50 @@
  * OTHER DEALINGS IN THE SOFTWARE.
 */
 
-var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-    video = document.getElementById('video');
+var canvas = document.getElementById('canvas')
+  var   context = canvas.getContext('2d')
+  var  video = document.getElementById('video');
+
+
+  /**
+   * https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+   * 直接调用play方法开始播放会报错误滴
+   */
+
+  /**
+   * media events
+   * https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
+   */
+
+  //  video.paused
+
+   video.addEventListener('play', function(e) {
+     console.log('play')
+     window.requestNextAnimationFrame(animate);
+   })
+
+   video.addEventListener('pause', function(e) {
+    console.log('pause')
+  })
+
+  video.addEventListener('loadeddata', function(e) {
+    console.log('loadeddata');
+  })
+
+  video.addEventListener('loadend', function(e) {
+    console.log('loadend');
+  })
+
+  video.addEventListener('loadstart', function(e) {
+    console.log('loadstart')
+  })
+  
+
+    console.log(111)
 
 function animate() {
-   if (!video.ended) {
+   if (!video.ended && !video.paused) {
      context.drawImage(video, 0, 0, canvas.width, canvas.height);
      window.requestNextAnimationFrame(animate);
    }
 }
-
-video.onload = function (e) {
-   video.play();
-   window.requestNextAnimationFrame(animate);
-};
-
-alert('This example plays a video, but due to copyright restrictions and size limitations, the video is not included in the code for this example. To make this example work, download a video, and replace the two source elements in example.html to refer to your video.');
